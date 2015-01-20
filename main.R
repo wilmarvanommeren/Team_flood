68a66745c2bb74be25c7fa238514ff8be8d5b974
wilmarvanommeren <wilmarvanommeren@hotmail.com>
2015-01-20 10:18:36 +0100

    4th commit: clumped flood area

--- main.R
+++ main.R
@@ -5,6 +5,7 @@
 library(sp)
 library(raster)
 library(RColorBrewer)
+library(igraph)
 
 # Load source scripts
 source('./r/fill.up.R')
@@ -14,7 +15,7 @@ source('./r/create.breach.R')
 DEM <- raster("data/AHN 5m/ahn_wageningen_5m.tif")
 
 # Filled up DEM, no breach
-waterheight <- 2#meter
+waterheight <- 7#meter
 DEM.filled <- fill.up(DEM, waterheight)
 
 # Plot flooded areas
@@ -29,4 +30,16 @@ breachRast <- create.breach(breachRast)
 DEMwithbreach<-mosaic(DEM,breachRast,fun=min)
 DEMwithbreach.filled <- fill.up(DEMwithbreach, waterheight)
 spplot (DEMwithbreach.filled,col.regions = colorPal, main='Waterheight [m]')
+
+# Clump flooded areas
+clumps.flood<-clump(DEMwithbreach.filled)
+clumps.floodEMP <- setValues(raster(clumps.flood), 1)#create empty raster
+clumps.floodEMP[is.na(clumps.flood)] <- NA #set NA values
+clumps.flood<-clump(clumps.floodEMP) #clump connected flooded area's
+
+# Select clumps connected to breach
+clump.intersect<-intersect(clumps.flood, breach)
+clumps.breach<-unique(clump.intersect@data@values)
+clumps.flood[clumps.flood!=clumps.breach]<-NA
+plot(clumps.flood)
 
