0f351df4a94c84f4508df3c76fd1f001686bf2b4
Wilmar van Ommeren <wilmarvanommeren@hotmail.com>
2015-01-24 16:18:27 +0100

    15th commit: created interactive map, changed plot

--- main.R
+++ main.R
@@ -15,16 +15,15 @@ source('./r/calculate.breach.area.R')
 source('./r/calculate.flooded.area.R')
 
 # Load data
-DEM <- raster("data/AHN 5m/ahn2_5_65az1.tif")
+DEM <- raster("data/AHNschouw/ahn2_5_64gn2.tif")
 
 # Project parameter(s)
 water.height <- 2   #meter
 plot(DEM)          #needed for the click
-no.of.breaches = 2
+no.of.breaches = 1
 breach.point <- click(n=no.of.breaches)
 breach.width = 150
 breach.height = 1
-
 
 # Calculate breach area
 breach.area<-calculate.breach.area(breach.point, breach.width)
@@ -33,9 +32,10 @@ breach.area<-calculate.breach.area(breach.point, breach.width)
 flooded.area <- calculate.flooded.area(breach.area, breach.height, water.height, DEM)
 
 # Plot flooded area
-Pallette <- colorRampPalette(brewer.pal(9, "Blues"))(20)
-spplot (flooded.area, col.regions = Pallette, 
+waterPallette <- colorRampPalette(brewer.pal(9, "Blues"))(20)
+DEMPallette<-colorRampPalette(c("darkseagreen","darkgreen","darkolivegreen","darkkhaki","darkgoldenrod"))(20)
+spplot (flooded.area, col.regions = waterPallette, 
         main='Flooded Area', sub='Waterheight [m]', 
         xlab='Longitude',ylab='Latitude', scales = list(draw = TRUE)
-        , sp.layout=list(list('sp.polygons', breach.area, col='red', first=FALSE)))
-
+        , sp.layout=list(list('sp.polygons', breach.area, col='red', first=FALSE)))+
+  as.layer(spplot(DEM, col.regions=DEMPallette), under=T)
