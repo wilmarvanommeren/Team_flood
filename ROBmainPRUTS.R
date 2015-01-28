1d28609c03b01418cf17f1d47a63405b51271c35
Rob Maas <robmaas77@hotmail.com>
2015-01-28 11:49:28 +0100

    35th commit: added scalebar and north arrow in raw version

--- ROBmainPRUTS.R
+++ ROBmainPRUTS.R
@@ -17,6 +17,8 @@ library(proj4)
 source('./r/fill.up.R')
 source('./r/calculate.breach.area.R')
 source('./r/calculate.flooded.area.R')
+source('./r/merge.breach.DEM.R')
+source('./r/create.openstreetmap.R')
 
 ## Load data
 # DEM <- getData('alt', country='NLD', mask=F)
@@ -37,38 +39,45 @@ breach.area<-calculate.breach.area(breach.point, breach.width)
 # Calculate flooded area
 flooded.area <- calculate.flooded.area(breach.area, breach.height, water.height, DEM)
 
-# Plot flooded area
+### Create (elements for) plot ###
+
+## Create openstreetmap basemap layer for plot
+osm <- create.openstreetmap(flooded.area)
+
+## Northarrow
+arrow.x <- extent(flooded.area)[1] + 0.02*(extent(flooded.area)[2]-extent(flooded.area)[1])
+arrow.y <- extent(flooded.area)[4] - 0.1*(extent(flooded.area)[4]-extent(flooded.area)[3])
+arrow.loc <- c(arrow.x,arrow.y)
+arrow.scale <- (extent(flooded.area)[4]-extent(flooded.area)[3])/10
+
+## Scalebar
+scale.x <- extent(flooded.area)[2] - 0.12*(extent(flooded.area)[2]-extent(flooded.area)[1])
+scale.y <- extent(flooded.area)[3] + 0.02*(extent(flooded.area)[4]-extent(flooded.area)[3])
+scale.loc <- c(scale.x,scale.y)
+scale.scale <- (extent(flooded.area)[2]-extent(flooded.area)[1])/10
+
+## Scalebar text
+# Text 1
+scale.text1.x <- scale.x
+scale.text1.y <- scale.y+0.02*(extent(flooded.area)[4]-extent(flooded.area)[3])
+scale.text1.loc <- c(scale.text1.x,scale.text1.y)
+scale.text1.txt <- "0km"
+
+# Text 2
+scale.text2.x <- extent(flooded.area)[2]- 0.03*(extent(flooded.area)[2]-extent(flooded.area)[1])
+scale.text2.y <- scale.y+0.02*(extent(flooded.area)[4]-extent(flooded.area)[3])
+scale.text2.loc <- c(scale.text2.x,scale.text2.y)
+scale.text2.txt <- paste(scale.scale,"km")
+
+## Colorpalettes
 waterPallette <- colorRampPalette(brewer.pal(9, "Blues"))(20)
 DEMPallette<-colorRampPalette(c("darkseagreen","darkgreen","darkolivegreen","darkkhaki","darkgoldenrod", "cornsilk","beige"))(20)
 
-
-### INSERTING OPEN STREET MAP ###
-# Extract the corners of the extent
-topleft <- cbind(extent(flooded.area)[1], extent(flooded.area)[4])   # top left coordinate of extent
-botright <- cbind(extent(flooded.area)[2], extent(flooded.area)[3])   # bottom right coordinate of extent
-
-# Make the extent-corners spatial and give it the same projection as the flooded area.
-extentcorners <- SpatialPoints(rbind(topleft, botright))
-proj4string(extentcorners) <- crs(flooded.area)
-
-# reproject extent to geographic coordinates to wgs84 for OpenStreetMap
-extent.wgs84<- spTransform(extentcorners, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
-
-# retrieve basemap
-osm <- openmap (c(bbox(extent.wgs84)[2,2], bbox(extent.wgs84)[1,1]), c(bbox(extent.wgs84)[2,1], bbox(extent.wgs84)[1,2]))
-
-# reproject basemap back to flooded area projection
-osm.trans <- openproj (osm, proj4string(extentcorners))
-
-#plot
-plot (osm.trans)
-plot (flooded.area, add=T, col=waterPallette)
-
-
-## plot flooded area
-# plot(flooded.area,add=T,col=waterPallette)
-# spplot (flooded.area, col.regions = waterPallette, 
-#         main='Flooded Area', sub='Waterheight [m]', 
-#         xlab='Longitude',ylab='Latitude', scales = list(draw = TRUE)
-#         , sp.layout=list(list('sp.polygons', breach.area, col='red', first=FALSE)))+
-#   as.layer(spplot(DEM, col=DEMPallette under=T))
+## Plot flooded area
+spplot (flooded.area, col.regions = waterPallette, main='Flooded Area', sub='Waterheight [m]', 
+        xlab='Longitude',ylab='Latitude', scales = list(draw = TRUE),
+        sp.layout=list(list('sp.polygons', breach.area, col='red', fill='red', first=FALSE),
+                       list("SpatialPolygonsRescale", layout.north.arrow(), offset=arrow.loc, scale=arrow.scale, fill=c('white','black')), 
+                       list("SpatialPolygonsRescale", layout.scale.bar(), offset=scale.loc, scale=scale.scale, fill=c('white','black')),
+                       list("sp.text", scale.text1.loc, scale.text1.txt, font=1, cex = 0.75),
+                       list("sp.text", scale.text2.loc, scale.text2.txt, font=1, cex = 0.75)))
